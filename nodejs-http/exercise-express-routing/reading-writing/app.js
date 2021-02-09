6b0bd482f0d18d67b07ad773e42d134db45b1924
Jess Pring <jessicapring@outlook.com>
2021-02-09 19:12:51 +0000

    refactored app.route

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -7,33 +7,31 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.get('/products', (req, res) => {
-  res.json(products);
-});
-
-app.post('/products', function (req, res) {
-  const newProduct = { ...req.body, id: products.length + 1 };
-  products = [...products, newProduct];
-  res.json(newProduct);
-});
-
-app.put('/products', function (req, res) {
-  let updatedProduct;
-  products = products.map(p => {
-    if (p.id === req.params.id) {
-      updatedProduct = { ...p, ...req.body };
-      return updatedProduct;
-    }
-    return p;
+app.route('/products')
+  .get((req, res) => {
+    res.json(products);
   })
-  res.json(updatedProduct);
-});
-
-app.delete('/products/:id', function (req, res) {
-  const deletedProduct = products.find(p => p.id === +req.params.id);
-  products = products.filter(p => p.id !== +req.params.id);
-  res.json(deletedProduct);
-});
+  .post((req, res) => {
+    const newProduct = { ...req.body, id: products.length + 1 };
+    products = [...products, newProduct];
+    res.json(newProduct);
+  })
+  .put((req, res) => {
+    let updatedProduct;
+    products = products.map(p => {
+      if (p.id === req.params.id) {
+        updatedProduct = { ...p, ...req.body };
+        return updatedProduct;
+      }
+      return p;
+    })
+    res.json(updatedProduct);
+  })
+  .delete((req, res) => {
+    const deletedProduct = products.find(p => p.id === +req.params.id);
+    products = products.filter(p => p.id !== +req.params.id);
+    res.json(deletedProduct);
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
