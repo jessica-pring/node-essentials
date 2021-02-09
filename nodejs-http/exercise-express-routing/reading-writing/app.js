b42e36729f151bc34afd562a0e6090c0e88b843f
Jess Pring <jessicapring@outlook.com>
2021-02-09 18:38:59 +0000

    post method implemented

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 let products = [];
 
 app.post('/products', function (req, res) {
-  // implement
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products = [...products, newProduct];
+  res.json(newProduct);
 });
 
 app.put('/products', function (req, res) {
